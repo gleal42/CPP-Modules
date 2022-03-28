0c7120bb5a99b723f355f087e8dbffac84b11cf1
Goncalo Leal <jobsgoncaloleal@gmail.com>
2022-03-28 21:49:08 +0100

    Exercise 00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,13 +6,27 @@
 /*   By: gleal <gleal@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 22:40:39 by gleal             #+#    #+#             */
-/*   Updated: 2022/03/27 22:42:32 by gleal            ###   ########.fr       */
+/*   Updated: 2022/03/28 21:41:44 by gleal            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
+# include <cctype>
 
-int main()
+int main(int argc, char **argv)
 {
-	std::cout << "Hello";
+	int	i = 0;
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (++i < argc)
+		{
+			int	j = 0;
+			while (argv[i][j])
+				std::cout << (char)std::toupper(argv[i][j++]);
+		}
+	}
+	std::cout << std::endl;
+	return 0;
 }
