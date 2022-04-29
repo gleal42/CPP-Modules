14467ff0987e21146ed3339913c1b4ef09c86b6c
gleal42 <jobsgoncaloleal@gmail.com>
2022-04-29 03:08:36 +0100

    added tests

--- C05/ex01/srcs/main.cpp
+++ C05/ex01/srcs/main.cpp
@@ -6,149 +6,111 @@
 /*   By: gleal <gleal@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 00:32:47 by gleal             #+#    #+#             */
-/*   Updated: 2022/04/28 03:13:39 by gleal            ###   ########.fr       */
+/*   Updated: 2022/04/29 03:04:53 by gleal            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
-void    testing_constructor_throws()
+void invalid_form_tests()
 {
     std::cout << std::endl;
-    std::cout << "Testing Constructor Throws" << std::endl;
+    std::cout << "Invalid Form Tests" << std::endl;
     std::cout << std::endl;
-    // Testing construction with grade too high (the smaller the number the higher the grade)
+    std::cout << "Grade to High examples" << std::endl;
     try {
-        Bureaucrat a("John", -1);
-        std::cout << a << std::endl;
+        Form c("Failed form", 0, 50);
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
-    // Testing construction with grade too low (the higher the number the lower the grade)
     try {
-        Bureaucrat b("Peter", 151);
-        std::cout << b << std::endl;
+        Form d("Failed form", 50, 0);
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
-    // Testing construction with mixed grades (invalid first)
-    try
-    {
-        Bureaucrat c("Rosario", 152);
-        std::cout << c << std::endl;
-        Bureaucrat d("Rui", 15);
-        std::cout << d << std::endl;
-    } 
-    catch (std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    // Testing construction with mixed grades (invalid last)
-    try
-    {
-        Bureaucrat e("Rogerio", 15);
-        std::cout << e << std::endl;
-        Bureaucrat f("Milton", 152);
-        std::cout << f << std::endl;
-    } 
-    catch (std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    // Testing construction with appropriate grade
     try {
-        Bureaucrat g("Orlando", 15);
-        std::cout << g << std::endl;
-        throw g;
+        Form e("Failed form", 0, 0);
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
-    } catch (Bureaucrat &temp) {
-        std::cout << temp << std::endl;
     }
-    // Testing copy construction with valid grades
-    try
-    {
-        Bureaucrat h("Rui", 15);
-        Bureaucrat i(h);
-    } 
-    catch (std::exception &e)
-    {
+    std::cout << "Grade too low examples" << std::endl;
+    try {
+        Form h("Failed form", 151, 2);
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    try {
+        Form i("Failed form", 2, 151);
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "Grade Mixed examples" << std::endl;
+    try {
+        Form f("Failed form", 0, 151);
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+    try {
+        Form g("Failed form", 151, 0);
+    } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
 }
 
-void    testing_increment_throws()
+void    bureaucratsigntests()
 {
     std::cout << std::endl;
-    std::cout << "Testing Increment Throws" << std::endl;
-    Bureaucrat a("Andy", 1);
-    Bureaucrat b("Holt", 2);
-    Bureaucrat c("Terry", 3);
-    try {
-        a.increment_grade();
-    } catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << a << std::endl;
-    try {
-        b.increment_grade();
-    } catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << b << std::endl;
-    try {
-        c.increment_grade();
-    } catch (std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-    std::cout << c << std::endl;
-}
+    std::cout << "Bureaucrat Signing tests" << std::endl;
+    std::cout << std::endl;
 
-void    testing_decrement_throws()
-{
+    Form a("President form", 10, 20);
+
+    std::cout << "Test 1 Bureaucrat with low level" << std::endl;
+    Bureaucrat pl ("Paulo", 15);
+    pl.signForm(a);
+    std::cout << a << std::endl;
+
+    std::cout << "Test 2 Bureaucrat sign level enough" << std::endl;
+    Bureaucrat mrs ("Marcelo Rebelo de Sousa", 1);
+    mrs.signForm(a);
+    std::cout << a << std::endl;
+
     std::cout << std::endl;
-    std::cout << "Testing Decrement Throws" << std::endl;
-    Bureaucrat a("Amy", 150);
-    Bureaucrat b("Rosa", 149);
-    Bureaucrat c("Gina", 148);
+    std::cout << "Test 3 signing twice" << std::endl;
     try {
-        a.decrement_grade();
+        pl.signForm(a);
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
-    std::cout << a << std::endl;
+    std::cout << std::endl;
+    Form b("Medium Form", 12, 30);
+    std::cout << "Test 4 try catch inside try catch" << std::endl;
     try {
-        b.decrement_grade();
+        pl.signForm(b);
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
     std::cout << b << std::endl;
+    std::cout << "Test 5 Signing directly (without bureacrat)" << std::endl;
     try {
-        c.decrement_grade();
+        b.beSigned(mrs);
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
-    std::cout << c << std::endl;
+    std::cout << b << std::endl;
+    std::cout << std::endl;
 }
 
 int main()
 {
     std::cout << "Basic Tests" << std::endl;
     std::cout << std::endl;
-    Bureaucrat a;
-    Bureaucrat b("Ben", 1);
+    Form a;
     std::cout << a << std::endl;
-    std::cout << a.getGrade() << std::endl;
+    Form b("Important form", 10, 5);
     std::cout << b << std::endl;
-    std::cout << b.getName() << std::endl;
-    std::cout << "Random = Ben" << std::endl;
-    a = b;
-    std::cout << "Assignment only copies grade because name is const" << std::endl;
-    std::cout << a << std::endl;
-    Bureaucrat c(a);
-    std::cout << c << std::endl;
-    testing_constructor_throws();
-    testing_increment_throws();
-    testing_decrement_throws();
-    std::cout << "yyyyyyyyo" << std::endl;
+
+    invalid_form_tests();
+    bureaucratsigntests();
 }
