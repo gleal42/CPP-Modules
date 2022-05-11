d1e10901a73c27158d7af31b7150ccb5749f2932
Goncalo Leal <jobsgoncaloleal@gmail.com>
2022-05-11 02:51:38 +0100

    added better tests and removed unnecessary conditional statements

--- C08/ex00/inc/easyfind.hpp
+++ C08/ex00/inc/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: gleal <gleal@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 18:14:55 by gleal             #+#    #+#             */
-/*   Updated: 2022/05/09 21:56:48 by gleal            ###   ########.fr       */
+/*   Updated: 2022/05/10 18:46:25 by gleal            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,13 @@
 #include <algorithm>
 #include <stdexcept>
 
-//  || *temp == *container.end()
-
 template <class T>
 typename T::iterator	easyfind(T &container, int i)
 {
 	typename T::iterator temp = std::find(container.begin(), container.end(), i);
-	if (temp != container.end())
-		return temp;
-	else
+	if (temp == container.end())
 		throw std::invalid_argument("Value not found");
+	return temp;
 }
 
 #endif
--- C08/ex00/srcs/main.cpp
+++ C08/ex00/srcs/main.cpp
@@ -6,17 +6,17 @@
 /*   By: gleal <gleal@student.42lisboa.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 19:10:00 by gleal             #+#    #+#             */
-/*   Updated: 2022/05/09 22:34:11 by gleal            ###   ########.fr       */
+/*   Updated: 2022/05/10 00:33:01 by gleal            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
-
-#include <vector>
-#include <array>
-#include <deque>
-#include <forward_list>
 #include <iostream>
+#include <vector>
+#include <deque>
+#include <list>
+#include <stack>
+#include <queue>
 
 void vec_tests()
 {
@@ -28,22 +28,6 @@ void vec_tests()
 	std::cout << *easyfind(vec, 3) << std::endl;
 	try {
 		std::cout << *easyfind(vec, 4) << std::endl;
-	} catch (std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	std::cout << std::endl;
-}
-
-void arr_tests()
-{
-	std::cout << "Array" << std::endl;
-	std::array<int, 3> arr = {1, 2, 3};
-	std::cout << *easyfind(arr, 1) << std::endl;
-	std::cout << *easyfind(arr, 2) << std::endl;
-	std::cout << *easyfind(arr, 3) << std::endl;
-	try {
-		std::cout << *easyfind(arr, 4) << std::endl;
 	} catch (std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
@@ -68,20 +52,16 @@ void deque_tests()
 	std::cout << std::endl;
 }
 
-void fwd_lst_tests()
+void lst_tests()
 {
-	std::cout << "Forward List" << std::endl;
-	std::forward_list<int> fl;
-	std::forward_list<int>::iterator it;
-
-	fl.assign(1,1);
-	fl.insert_after(1,1);
-
-	std::cout << *easyfind(fl, 1) << std::endl;
-	std::cout << *easyfind(fl, 2) << std::endl;
-	std::cout << *easyfind(fl, 3) << std::endl;
+	std::cout << "List" << std::endl;
+	int v[] = {1, 2, 3};
+	std::list<int> lst(v, v + sizeof(v)/sizeof(int));
+	std::cout << *easyfind(lst, 1) << std::endl;
+	std::cout << *easyfind(lst, 2) << std::endl;
+	std::cout << *easyfind(lst, 3) << std::endl;
 	try {
-		std::cout << *easyfind(fl, 4) << std::endl;
+		std::cout << *easyfind(lst, 4) << std::endl;
 	} catch (std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
@@ -89,14 +69,29 @@ void fwd_lst_tests()
 	std::cout << std::endl;
 }
 
+void bad_tests()
+{
+	std::stack<int> first;
+	for (int i=1; i<4; ++i)
+		first.push(i);
+	// std::cout << *easyfind(first, 1) << std::endl;
+	std::queue<int> sec;
+	for (int i=1; i<4; ++i)
+		sec.push(i);
+	// std::cout << *easyfind(sec, 1) << std::endl;
+	std::priority_queue<int> thrd;
+	for (int i=1; i<4; ++i)
+		thrd.push(i);
+	// std::cout << *easyfind(thrd, 1) << std::endl;
+	int v[] = {1, 2, 3};
+	(void)v;
+	// std::cout << *easyfind(v, 1) << std::endl;
+}
+
 int main()
 {
 	vec_tests();
-	arr_tests();
 	deque_tests();
-	fwd_lst_tests();
-
-
-	// std::array<int> arr(v, v + sizeof(v)/sizeof(int));
-
+	lst_tests();
+	bad_tests();
 }
